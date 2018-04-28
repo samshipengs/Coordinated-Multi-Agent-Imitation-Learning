174450ceadefb7ddcd6ab507c714342e6d3001a0
samshipengs <samshipengs@hotmail.com>
2018-04-28 12:54:47 -0400

    Switched to full role-aglinment scheme.

--- code/helpers_old.py
+++ code/helpers_old.py
@@ -100,15 +100,16 @@ def id_teams(event_dfs):
             result[id2] = name2
     return result
 
+# # chunk to sequences
+# def get_sequences(X, length, dim):
+#     ''' 
+#     segment a given list of moments to list of chunks each with size length.
+#     this is usually used before creating batches.
+#     '''
+#     n_segs = len(X)//length
+#     return np.array([X[i*length:(i+1)*length] for i in range(0, n_segs)]).reshape(-1, length, dim)
 
 def get_sequences(single_game, sequence_length, overlap):
-    ''' create events where each event is a list of sequences from
-        single_game with required sequence_legnth and overlap
-
-        single_game: A list of events
-        sequence_length: the desired length of each event (a sequence of moments)
-        overlap: how much overlap wanted for the sequence generation
-    '''
     train = []
     target = []
     for i in single_game:
@@ -129,12 +130,13 @@ def get_sequences(single_game, sequence_length, overlap):
     return train, target
 
 
+# chunk to batch
+# define ftn that generates batches
 def get_minibatches(inputs, targets, batchsize, shuffle=False):
     '''
-        inputs: A list of events where each event is a sequence (array) of moments
-                with sequence_length
-        targets: target created by shifting 1 from inputs
-        batchsize: desired batch size
+        inputs:
+        targets:
+        batchsize:
         shuffle: Shuffle input data
 
         return: array of data in batch, shape:  
@@ -154,7 +156,7 @@ def get_minibatches(inputs, targets, batchsize, shuffle=False):
         target_batches.append(targets[excerpt])
     return np.array(batches), np.array(target_batches)
 
-
+# define ftn that generates batches
 def iterate_minibatches(inputs, targets, batchsize, shuffle=False):
     '''
         same as get_minibatches, except returns a generator
@@ -169,6 +171,41 @@ def iterate_minibatches(inputs, targets, batchsize, shuffle=False):
         else:
             excerpt = slice(start_idx, start_idx + batchsize)
         yield inputs[excerpt], targets[excerpt]
+
+
+def order_moment(m, rm, ro, extreme=3):
+    '''
+        m: moments, rm: role model, ro: role order
+        for the case of multiple players sharing the smae roles,
+        (this can happen to even with hiddlen structure learning, 
+         although it might be allevaited by using lienar assignment)
+        so for now, we come up with an extrem case where same role are occupied by say, 3 players,
+        then we still follow the meta order but create paddings 3 times.
+    '''
+    # reorder moments by role based mapping, where first col is player id
+    role = [rm[int(i)][0] for i in m[:,0]]
+    u_role = list(set(role))
+    assert len(u_role) >= 2, 'it goes over extreme case'
+    
+    d1,d2 = m.shape
+    try:
+        assert d1 == 5, 'd1,d2 = {0:}, {1:}'.format(d1, d2)
+    except:
+        print('Warning:', d1, d2, end='\r')
+    # initialize slots (5 meta positions)
+    slots = np.zeros((extreme*5, d2))
+    counter = {}
+    for i in range(len(role)):
+        role_i = role[i]
+        if role_i not in counter.keys():
+            counter[role_i] = 0
+        else:
+            # note: this could possibly be better if add linear assignment
+            counter[role_i] += 1
+        # filling in the slots
+        slots[ro[role_i]*extreme+counter[role_i], :] = m[i, :]
+#     return slots[:, 1:] # [, 1:] slice 1 since we don't need the player id anymore
+    return slots[:, 1:] 
 
 
 class OneHotEncoding:
@@ -193,6 +230,185 @@ class OneHotEncoding:
         one_hot_targets = np.eye(nb_classes)[targets]
         # print(one_hot_targets)
         return one_hot_targets
+    
+def process_moments(moments, homeid, awayid, id_role, role_order, court_index, game_id, extreme=3):
+    '''
+        moments: a list of moment(frame)
+        homeid: home team's team_id
+        awayid: visit team's team_id
+        id_role: a dictionary contains mapping from player id to a list of roles,
+            e.g. {2200: ['C-F'], 2449: ['F'], ... }
+        role_order: a dictionary contains mapping from role to order,
+            e.g. {'F': 0, 'G':4, 'C-F':1, 'G-F':3, 'F-G':3, 'C':2, 'F-C':1}
+        court_index: the index indicating which team from the array is the team
+            on the left (i.e. close to the origin (0,0), this swaps in the second-half)
+        game_id: id of the game
+
+        return: a reordered moments array with defend team at first
+    '''
+    # init one hot encoding
+    ohe = OneHotEncoding()
+    result = []
+    shot_clock = []
+    # half court = 94/2
+    half_court = 47.
+    n_balls_missing = 0
+    for i in range(len(moments)):
+        # get quarter number
+        quarter_number = moments[i][0]
+
+        # ball position array
+        dm = len(moments[i][5])
+        player_ind = -1
+        if dm == 11: # ball is present
+            ball = np.array([moments[i][5][0][2:]])
+            player_ind = 1
+        elif dm == 10 and moments[i][5][0][:2] != [-1,-1]: # ball is not present
+            # print('Ball is not present.')
+            n_balls_missing += 1
+            ball = np.array([[-1, -1, -1]])
+            player_ind = 0
+        else:
+            print('Warning!: There are less than 10 players! (skip)')
+            continue
+        # get player position data
+        pp = np.array(moments[i][5][player_ind:])
+        
+        # home (update: instead of using home/visitor, we will just follow court index)
+        # ignore last null column
+        hpp = pp[:5, :-1] # team1
+        # visitor
+        vpp = pp[5:, :-1] # team2
+        # add one hot encoding for the teams
+        h_team = hpp[:, 0]
+        v_team = vpp[:, 0]
+        # stack on the one hot encoding and leave the team id
+        hpp = np.column_stack((hpp[:, 1:], ohe.encode(h_team)))
+        vpp = np.column_stack((vpp[:, 1:], ohe.encode(v_team)))
+        
+        h = order_moment(hpp, id_role, role_order)
+        v = order_moment(vpp, id_role, role_order)
+        # note, the player_id is discarded after order_moment
+
+        # combine home and visit => update: in defend then offend order
+        game_id = int(game_id)
+        hv = []
+        if quarter_number <= 2: # first half
+            if court_index[game_id] == 0:
+                # all the left players on the left side,
+                # and the right court players also on the left side
+                if sum(hpp[:, 1]<=half_court)==5 and sum(vpp[:, 1]<=half_court)==5:
+                    hv = np.vstack((h,v))
+                else:
+                    continue
+            else:
+                # all the left players on the right side,
+                # and the right court players also on the right side
+                if sum(hpp[:, 1]>=half_court)==5 and sum(vpp[:, 1]>=half_court)==5:
+                    # we also normalize the court i.e. move back to the left court (mirror reflect)
+                    v[:, 1] = 2*half_court - v[:, 1]
+                    h[:, 1] = 2*half_court - h[:, 1]
+                    hv = np.vstack((v,h))
+                else:
+                    continue
+        elif quarter_number > 2: # second half the court position is switched
+            if court_index[game_id] == 0:
+                # all the left players on the left side,
+                # and the right court players also on the left side
+                if sum(hpp[:, 1]<=half_court)==5 and sum(vpp[:, 1]<=half_court)==5:
+                    # now the defend team is the team2, v
+                    hv = np.vstack((v,h))
+                else:
+                    continue
+            else:
+                # all the left players on the right side,
+                # and the right court players also on the right side
+                if sum(hpp[:, 1]>=half_court)==5 and sum(vpp[:, 1]>=half_court)==5:
+                    h[:, 1] = 2*half_court - h[:, 1]
+                    v[:, 1] = 2*half_court - v[:, 1]
+                    hv = np.vstack((h,v))
+                else:
+                    continue
+        if len(hv) == 0:
+            continue
+
+        # also record shot clocks for each of the moment/frame, this is used to
+        # seperate a sequence into different frames (since when shot clock resets,
+        # it usually implies a different state of game)
+        shot_clock.append(moments[i][3])
+
+        # only get the necessary parts
+        ohe_i = list(hv[0][2:]) + list(hv[extreme*5][2:]) # first team's (defending) one hot concat with second 
+        resulti = hv[:, :2] # just the player's position
+        # stack on the ball position
+        resulti = list(np.array(resulti).reshape(-1)) + list(ball.reshape(-1)) + ohe_i
+        result.append(resulti)
+
+    # if n_balls_missing!=0: print('n_balls_missing:', n_balls_missing)
+    
+    if len(result) == 0:
+        return None
+    else:
+        return np.array(result), shot_clock         
+
+
+def get_game_data(events, id_role, role_order, court_index, game_id, event_threshold=10, subsample_factor=3):
+    '''
+        events: a single game's events dataframe
+        
+        return: a list of events, each event consists a sequence of moments 
+    '''
+    # 
+    homeid = events.loc[0].home['teamid']
+    awayid = events.loc[0].visitor['teamid']
+    single_game = []
+    # sc = 24. # init 24s shot clock
+
+    # filter out seq length less than threshold, this has to be greater than 2
+    # otherwise, there might be duplicates appearing
+    len_th = event_threshold    # the number of moments in a single event need to satisfy
+    n = 0    # record number that satisfies the threshold
+    n_short = 0    # number that doesnt match
+    for k, v in enumerate(events.moments.values):
+        result_i = process_moments(v, homeid, awayid, id_role, role_order, court_index, game_id)
+        if result_i == None:
+            continue
+        else:
+            s1 = 0 # index that divides the sequence, this usually happens for 24s shot clock
+            pm, scs = result_i
+            for i in range(len(scs)-1):
+                # sometimes there are None shot clock value
+                if scs[i] != None and scs[i+1] == None:
+                    if len(scs[s1:i+1]) >= len_th:
+                        single_game.append(pm[s1:i+1])
+                        n += 1
+                    else:
+                        n_short += 1
+                    s1 = i+1
+                elif scs[i] == None:
+                    s1 += 1
+                elif scs[i+1] >= scs[i]:
+                    if len(scs[s1:i+1]) >= len_th:
+                        single_game.append(pm[s1:i+1])
+                        n += 1
+                    else:
+                        n_short += 1
+                    s1 = i+1
+            # grab the end piece
+            if s1 != len(scs)-2:
+                if len(scs[s1:]) >= len_th:
+                    single_game.append(pm[s1:])
+                    n += 1
+                else:
+                    n_short += 1
+    # dimensions extreme<3> x n_players<10> x (player_pos<2> + teamid_onehot<25> + ball<3>) = 900
+    # dimensions = extreme<3> x n_players<10> x player_pos<2> + teamid_onehot<4> + ball<3> = 67
+
+    if subsample_factor != 0: # do subsample
+        print('subsample enabled with subsample factor', subsample_factor)
+        return [subsample_sequence(m, subsample_factor) for m in single_game]
+    else:
+        return single_game#, (n, n_short)
 
 
 def filter_event_type(events_df, discard_event):
@@ -266,6 +482,8 @@ def get_velocity(event, fs, mode=0):
             start_ind += 2
         return combined
             
+
+
 
 # =============================================================================================
 # ROLE ALIGNMENT
@@ -531,13 +749,12 @@ class EmissionVis:
 
 class RoleAssignment:
     ''' main class for assigning hiden rows based on HMM and linear assignment'''
-    def __init__(self, n_iter, verbose):
-        self.n_iter = n_iter
-        self.verbose = verbose
+    def __init__(self):
+        pass
 
     def train_hmm(self, X, lengths, n_comp, n_mix):
         # model = hmm.GMMHMM(n_components=n_comp, n_mix=n_mix, verbose=True, n_iter=30)
-        model = hmm.GaussianHMM(n_components=n_comp, verbose=self.verbose, n_iter=self.n_iter, random_state=42)
+        model = hmm.GaussianHMM(n_components=n_comp, verbose=True, n_iter=50, random_state=42)
         model.fit(X, lengths)
         
         # get the means 
