6fcf9ff0a376baff052c8706dde9879591f47ad2
Sam Shi <Sam.Shi@MLSE.com>
2018-05-01 16:18:58 -0400

    Create compact SinglePolicy class for easier training and prepare for joint training

--- code/model.py
+++ code/model.py
@@ -1,8 +1,10 @@
 import tensorflow as tf
 from tensorflow.python.ops.rnn import _transpose_batch_time
+from datetime import datetime
+import os
 
 
-def dynamic_raw_rnn(cell, input_, batch_size, seq_length, horizon, output_dim):
+def dynamic_raw_rnn(cell, input_, batch_size, seq_length, horizon, output_dim, policy_number):
     # raw_rnn expects time major inputs as TensorArrays
     inputs_ta = tf.TensorArray(dtype=tf.float32, size=seq_length)
     inputs_ta = inputs_ta.unstack(_transpose_batch_time(input_))  # model_input is the input placeholder
@@ -10,6 +12,7 @@ def dynamic_raw_rnn(cell, input_, batch_size, seq_length, horizon, output_dim):
     input_dim = input_.get_shape()[-1].value  # the dimensionality of the input to each time step
     output_dim = output_dim  # the dimensionality of the model's output at each time step
 
+    player_fts = 4
     def loop_fn(time, cell_output, cell_state, loop_state):
         # check if finished 
         elements_finished = (time >= seq_length)
@@ -29,7 +32,9 @@ def dynamic_raw_rnn(cell, input_, batch_size, seq_length, horizon, output_dim):
                                  lambda: tf.zeros([batch_size, input_dim], dtype=tf.float32), 
                                  lambda: tf.cond(tf.equal(tf.mod(time, horizon+1), tf.constant(0)),
                                                  lambda: inputs_ta.read(time),
-                                                 lambda: tf.concat((emit_output, inputs_ta.read(time)[:, 2:]), axis=1)))
+                                                 lambda: tf.concat((inputs_ta.read(time)[:, :policy_number*player_fts],
+                                                                    emit_output, 
+                                                                    inputs_ta.read(time)[:, policy_number*player_fts+2:]), axis=1)))
         next_loop_state = None
         return (elements_finished, next_input, next_cell_state, emit_output, next_loop_state)
     
@@ -38,6 +43,113 @@ def dynamic_raw_rnn(cell, input_, batch_size, seq_length, horizon, output_dim):
     final_state = last_state
 
     return outputs, final_state
+
+
+class SinglePolicy:
+    def __init__(self, policy_number, state_size, batch_size, input_dim, output_dim,
+                 learning_rate, seq_len, l1_weight_reg = False, logs_path = './train_logs/'):
+        tf.reset_default_graph()
+        # use training start time as the unique naming
+        train_time = datetime.now().strftime('%Y_%m_%d_%H_%M_%S')
+
+        # hyper-parameters
+        self.state_size = state_size
+        self.batch_size = batch_size
+        self.dimx = input_dim # 4(x,y,vx,vy)*14 + 6 = 62
+        self.dimy = output_dim 
+        self.learning_rate = learning_rate
+        self.seq_len = seq_len 
+        self.policy_number = policy_number
+
+        # lstm cells
+        lstm1 = tf.contrib.rnn.BasicLSTMCell(state_size, forget_bias=1.)
+        # lstm1 = tf.nn.rnn_cell.DropoutWrapper(lstm1, output_keep_prob=0.8)
+
+        lstm2 = tf.contrib.rnn.BasicLSTMCell(state_size, forget_bias=1.)
+        # lstm2 = tf.nn.rnn_cell.DropoutWrapper(lstm2, output_keep_prob=0.8)
+
+        lstm_cell = tf.contrib.rnn.MultiRNNCell([lstm1, lstm2])
+
+        # input placeholders
+        self.h = tf.placeholder(tf.int32, name='horizon')
+        self.X = tf.placeholder(tf.float32, [self.batch_size, self.seq_len, self.dimx], name = 'train_input')
+        self.Y = tf.placeholder(tf.float32, [self.batch_size, self.seq_len, self.dimy], name = 'train_label')
+
+        # rnn structure
+        output, last_states = dynamic_raw_rnn(cell = lstm_cell, 
+                                        input_ = self.X,
+                                        batch_size = self.batch_size,
+                                        seq_length = self.seq_len,
+                                        horizon = self.h,
+                                        output_dim = self.dimy, 
+                                        policy_number=self.policy_number)
+
+        # output as the prediction
+        self.pred = tf.identity(output, name='prediction')
+
+        # tensorboard's graph visualization more convenient
+        with tf.name_scope('MSEloss'):
+            if l1_weight_reg:
+                # loss (also add regularization on params)
+                tv = tf.trainable_variables()
+                # l1 loss
+                l1_regularizer = tf.contrib.layers.l1_regularizer(scale=0.005, scope=None)
+                regularization_cost = tf.contrib.layers.apply_regularization(l1_regularizer, tv)
+                self.loss = tf.identity(tf.losses.mean_squared_error(self.Y, self.pred) + regularization_cost, name='loss')
+            else:
+                # no weight loss
+                self.loss = tf.losses.mean_squared_error(self.Y, self.pred)
+
+        with tf.name_scope('Adam'):
+            # optimzier
+            self.opt = tf.train.AdamOptimizer(self.learning_rate).minimize(self.loss)
+    
+        # initialize variables
+        init = tf.global_variables_initializer()
+        # create a summary to monitor cost tensor
+        self.train_summary = tf.summary.scalar("TrainMSEloss", self.loss)
+        self.valid_summary = tf.summary.scalar("ValidMSEloss", self.loss)
+        # # Merge all summaries into a single op
+        # merged_summary_op = tf.summary.merge_all()
+
+        self.saver = tf.train.Saver()
+        # session
+        self.sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+        # initializing the variables
+        self.sess.run(init)
+        # op to write logs to Tensorboard
+        self.train_writer = tf.summary.FileWriter(logs_path+'/train'+train_time, graph=tf.get_default_graph())
+        self.valid_writer = tf.summary.FileWriter(logs_path+'/valid'+train_time, graph=tf.get_default_graph())
+
+    def train(self, train_xi, train_yi, k):
+        return self.sess.run([self.pred, self.loss, self.opt, self.train_summary], 
+                             feed_dict={self.X: train_xi, self.Y: train_yi, self.h: k})
+    
+    def validate(self, val_xi, val_yi, k):
+        return self.sess.run([self.loss, self.valid_summary], 
+                             feed_dict={self.X: val_xi, self.Y: val_yi, self.h: k})
+
+    def save_model(self, models_path):
+        # save model
+        save_path = models_path + 'policy{0:}'.format(self.policy_number) + '/model'
+        if not os.path.exists(models_path):
+            os.makedirs(models_path)
+        self.saver.save(self.sess, save_path)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # https://bretahajek.com/2017/04/importing-multiple-tensorflow-models-graphs/
