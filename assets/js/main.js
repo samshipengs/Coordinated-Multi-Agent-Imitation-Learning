422ad44d1c4c9b21751f6d89c301a00d6babafa9
Sam Shi <Sam.Shi@MLSE.com>
2018-06-28 10:58:35 -0400

    Modify page with attempt to get better side nav.

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,3 +31,19 @@ $(function() {
 
   $('img').on('load', sectionHeight);
 });
+
+$(document).ready(function() {
+  
+  $(window).scroll(function () {
+      //if you hard code, then use console
+      //.log to determine when you want the 
+      //nav bar to stick.  
+      console.log($(window).scrollTop())
+    if ($(window).scrollTop() > 280) {
+      $('nav ul').addClass('navbar-fixed');
+    }
+    if ($(window).scrollTop() < 281) {
+      $('nav ul').removeClass('navbar-fixed');
+    }
+  });
+});
